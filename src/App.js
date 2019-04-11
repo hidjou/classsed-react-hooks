fd8e3f6844a03fd31726ba48eb170c09018c9f72
Ahmed Hadjou <thehidjou@yahoo.com>
2019-04-11 12:35:22 +0100

    Part 1 - useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  const [people, setPeople] = useState([
+    {
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Doe'
+    }
+  ]);
+  const [person, setPerson] = useState({ firstName: '', lastName: '' });
+  const onChange = (event) => {
+    setPerson({ ...person, [event.target.name]: event.target.value });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (person.firstName.trim() === '' || person.lastName.trim() === '') return;
+
+    const newPerson = {
+      firstName: person.firstName.trim(),
+      lastName: person.lastName.trim()
+    };
+
+    setPeople([...people, newPerson]);
+    setPerson({ firstName: '', lastName: '' });
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h2>Add a person: </h2>
+          <hr />
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="firstName"
+                placeholder="First Name.."
+                value={person.firstName}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="lastName"
+                placeholder="Last Name.."
+                value={person.lastName}
+                onChange={onChange}
+              />
+            </div>
+            <button className="btn btn-success" type="submit">
+              Add person
+            </button>
+          </form>
+        </div>
+        <div className="col">
+          <h2>People: </h2>
+          <hr />
+          {people.map((p) => (
+            <div key={Math.random() * 1000000000}>
+              <p>
+                {p.firstName} {p.lastName}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
